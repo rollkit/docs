827c03df4736e546e1e23326eb8998835d57185a
joshcs.eth ᵍᵐ <mail@jcstein.dev>
2023-01-23 17:56:47 -0600

    doc: remove `.md` from `<Link>`

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
            <Link
             className="button button--secondary button--lg"
-            to="/docs/intro.md">
+            to="/docs/intro">
             Build Modular
           </Link>
           {/* <Link
