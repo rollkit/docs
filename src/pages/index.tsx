294322cd5039b0c7a371ba1d77d376d8b7c412fd
joshcs.eth ᵍᵐ <mail@jcstein.dev>
2023-01-30 12:24:55 -0500

    doc: change Build Modular -> Learn More

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ function HomepageHeader() {
            <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Build Modular
+            Learn More
           </Link>
           {/* <Link
             className="front-page-link"
