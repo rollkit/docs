09561c428415be4e962795d95ffff243751ec57d
joshcs.eth ᵍᵐ <mail@jcstein.dev>
2023-02-20 10:15:39 -0500

    remove title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">Be free.</h1>
+        {/* <h1 className="hero__title">{siteConfig.tagline}</h1> */}
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
            <Link
