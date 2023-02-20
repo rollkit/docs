3b1830a458213eb8fbe3fa9c86b2299fdc19c031
joshcs.eth ᵍᵐ <mail@jcstein.dev>
2023-02-20 10:29:04 -0500

    h1 size

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,8 +11,8 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        {/* <h1 className="hero__title">{siteConfig.tagline}</h1> */}
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <h1 className="hero__title">{siteConfig.tagline}</h1>
+        {/* <p className="hero__subtitle">{siteConfig.tagline}</p> */}
         <div className={styles.buttons}>
            <Link
             className="button button--secondary button--lg"
