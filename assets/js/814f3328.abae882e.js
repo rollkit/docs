b501349eac6562f5de6c61474d4bc0441d70d996
jcstein <mail@jcstein.dev>
2023-02-21 18:16:40 +0000

    deploy: c2667b56d76bb08c75e817eb1ab626f490a55f54

---
+++ assets/js/814f3328.abae882e.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[535],{5641:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Introducing Rollkit - a modular rollup framework","permalink":"/blog/introducing-rollkit"}]}')}}]);
