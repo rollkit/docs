b6cf849f679af623356102ac6db0722d4b62b544
joshcs.eth ᵍᵐ <mail@jcstein.dev>
2023-08-15 12:11:14 -0400

    v1

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,16 +1,51 @@
 // https://vitepress.dev/guide/custom-theme
-import { h } from 'vue'
+import { h, ref, onMounted, watch } from 'vue'
 import Theme from 'vitepress/theme'
 import './style.css'
 
+const currentTime = new Date().getTime(); // get current timestamp
+const hideUntil = localStorage.getItem('hideBannerUntil'); // retrieve stored timestamp
+
 export default {
-  extends: Theme,
+  ...Theme, 
   Layout: () => {
+    // A reactive property to determine if the banner should be shown
+    const showBanner = ref(true)
+
+    // Check sessionStorage when the component is mounted
+    onMounted(() => {
+      const hideBanner = sessionStorage.getItem('hideBanner')
+      if (hideBanner) {
+        showBanner.value = false
+      }
+    })
+
+    // Watch for changes in showBanner's value
+    watch(showBanner, (newValue) => {
+      if (!newValue) {
+        sessionStorage.setItem('hideBanner', 'true')
+      }
+    })
+
+    // Check if we have passed the 24 hours since the banner was closed
+    if (hideUntil && currentTime < parseInt(hideUntil)) {
+      showBanner.value = false;
+    }
+    
+    const closeBanner = () => {
+      const hideTime = new Date().getTime() + 24 * 60 * 60 * 1000; // current time + 24 hours in milliseconds
+      localStorage.setItem('hideBannerUntil', hideTime.toString());
+      showBanner.value = false;
+    }
+
     return h(Theme.Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
+      'layout-top': () => 
+        showBanner.value 
+        ? h('div', { class: 'layout-top' }, [
+            'This site is under construction 🏗️',
+            h('button', { class: 'close-button', onClick: closeBanner }, 'X')
+          ]) 
+        : null
     })
   },
-  enhanceApp({ app, router, siteData }) {
-    // ...
-  }
 }
