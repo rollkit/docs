bfb306761e30b0c1259fb06a9dec2b6f0ccd9809
joshcs.eth ᵍᵐ <mail@jcstein.dev>
2023-08-17 17:45:11 -0400

    test title

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -5,13 +5,15 @@ const telegramSVG = ` <svg width="24" height="24" viewBox="0 0 24 24" fill="none
 </svg>`;
 
 const { BASE: base = "/" } = process.env;
+const title = "Rollkit";
+const description = "The open modular framework for sovereign rollups.";
 
 // https://vitepress.dev/reference/site-config
 export default withMermaid({
   lang: 'en-US',
-  title: "Rollkit",
-  titleTemplate: ':title',
-  description: "The open modular framework for sovereign rollups.",
+  title: title,
+  titleTemplate: `:title · ${title}`,
+  description: description,
   lastUpdated: true,
   cleanUrls: true,
   ignoreDeadLinks: true,
@@ -51,7 +53,7 @@ export default withMermaid({
     ['meta', { name: 'msapplication-TileColor', content: '#fff' }],
     ['meta', { name: 'theme-color', content: '#fff' }],
     ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' }],
-    ['meta', { property: 'og:title', content: 'Rollkit' }],
+    ['meta', { property: 'og:title', content: `${title}` }],
     ['meta', { property: 'og:description', content: 'The open modular framework for sovereign rollups.' }],
     ['meta', { property: 'description', content: 'The open modular framework for sovereign rollups.' }],
     ['meta', { httpEquiv: 'Content-Language', content: 'en' }],
