0d146704cdf35ea38c5ecc07e86ada974a0f2c18
joshcs.eth ᵍᵐ <mail@jcstein.dev>
2023-08-22 12:46:45 -0400

    add dynamic og:title

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -51,7 +51,7 @@ export default withMermaid({
     ['meta', { name: 'msapplication-TileColor', content: '#fff' }],
     ['meta', { name: 'theme-color', content: '#fff' }],
     ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' }],
-    ['meta', { property: 'og:title', content: 'Rollkit' }],
+    // ['meta', { property: 'og:title', content: 'Rollkit' }],
     ['meta', { property: 'og:description', content: 'The open modular framework for sovereign rollups.' }],
     ['meta', { property: 'description', content: 'The open modular framework for sovereign rollups.' }],
     ['meta', { httpEquiv: 'Content-Language', content: 'en' }],
@@ -116,7 +116,20 @@ export default withMermaid({
     ]
 
   
-  }
+  },
+  transformPageData(pageData) {
+    pageData.frontmatter.head ??= [];
+    pageData.frontmatter.head.push([
+      'meta',
+      {
+        name: 'og:title',
+        content:
+          pageData.frontmatter.layout === 'home'
+            ? `Rollkit`
+            : `${pageData.title} | Rollkit`,
+      },
+    ]);
+  },
 })
 
 function nav() {
