0b4c11a3c1bb7a9a96a7fe45618a4f6a57110ab0
joshcs.eth <46639943+jcstein@users.noreply.github.com>
2023-10-13 14:36:53 -0400

    feat: add "yarn import-readme" script

---
+++ importReadme.mjs
@@ -0,0 +1,26 @@
+import fs from 'fs';
+import fetch from 'node-fetch';
+
+const filesToImport = [
+  {
+    url: 'https://raw.githubusercontent.com/jcstein/rollmint/main/README.md',
+    fileName: 'building-with-rollkit.md',
+  },
+];
+
+async function importReadme(file) {
+  try {
+    const response = await fetch(file.url);
+    if (response.ok) {
+      const markdown = await response.text();
+      fs.writeFileSync(`./learn/${file.fileName}`, markdown);
+      console.log(`Markdown file '${file.fileName}' successfully imported!`);
+    } else {
+      console.error(`Error fetching the markdown file: ${response.statusText}`);
+    }
+  } catch (error) {
+    console.error(`Error importing the markdown file: ${error.message}`);
+  }
+}
+
+filesToImport.forEach(importReadme);
