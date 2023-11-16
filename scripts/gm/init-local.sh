7ecce38b36e55b5706bfe7844330c36fb6d3e9dd
Manav Aggarwal <manavaggarwal1234@gmail.com>
2023-11-16 18:56:30 +0300

    Add restart script logic

--- scripts/gm/init-local.sh
+++ scripts/gm/init-local.sh
@@ -77,6 +77,14 @@ gmd gentx $KEY_NAME $STAKING_AMOUNT --chain-id $CHAIN_ID --keyring-backend test
 # collect genesis transactions
 gmd collect-gentxs
 
+# create a restart-local.sh file to restart the chain later
+rm restart-local.sh
+echo "DA_BLOCK_HEIGHT=$DA_BLOCK_HEIGHT" >> restart-local.sh
+echo "NAMESPACE_ID=$NAMESPACE" >> restart-local.sh
+echo "AUTH_TOKEN=$AUTH_TOKEN" >> restart-local.sh
+
+echo "gmd start --rollkit.aggregator true --rollkit.da_layer celestia --rollkit.da_config='{\"base_url\":\"http://localhost:26658\",\"timeout\":60000000000,\"fee\":600000,\"gas_limit\":6000000,\"auth_token\":\"'\$AUTH_TOKEN'\"}' --rollkit.namespace_id \$NAMESPACE --rollkit.da_start_height \$DA_BLOCK_HEIGHT --rpc.laddr tcp://127.0.0.1:36657 --p2p.laddr \"0.0.0.0:36656\"" >> restart-local.sh
+
 # start the chain
 gmd start --rollkit.aggregator true --rollkit.da_layer celestia --rollkit.da_config='{"base_url":"http://localhost:26658","timeout":60000000000,"fee":600000,"gas_limit":6000000,"auth_token":"'$AUTH_TOKEN'"}' --rollkit.namespace_id $NAMESPACE --rollkit.da_start_height $DA_BLOCK_HEIGHT --rpc.laddr tcp://127.0.0.1:36657 --p2p.laddr "0.0.0.0:36656"
 
