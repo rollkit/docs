3fb2cc8c71cc1f7e681c46d5fa10f5faf45a866b
Josh Stein <46639943+jcstein@users.noreply.github.com>
2023-11-16 11:23:39 -0500

    Apply suggestions from code review

--- scripts/gm/init-local.sh
+++ scripts/gm/init-local.sh
@@ -80,7 +80,7 @@ gmd collect-gentxs
 # export the Celestia light node's auth token to allow you to submit
 # PayForBlobs to Celestia's data availability network
 # this is for Arabica, if using another network, change the network name
-AUTH_TOKEN=$(celestia light auth write)
+AUTH_TOKEN=$(docker exec $(docker ps -q)  celestia bridge --node.store /home/celestia/bridge/ auth admin)
 
 # create a restart-local.sh file to restart the chain later
 [ -f restart-local.sh ] && rm restart-local.sh
