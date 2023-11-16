3044c089fa3697bc2cf5a923a685c391ac37c087
Josh Stein <46639943+jcstein@users.noreply.github.com>
2023-11-16 11:22:21 -0500

    Apply suggestions from code review

--- scripts/gm/init-local.sh
+++ scripts/gm/init-local.sh
@@ -85,7 +85,7 @@ AUTH_TOKEN=$(celestia light auth write)
 # create a restart-local.sh file to restart the chain later
 [ -f restart-local.sh ] && rm restart-local.sh
 echo "DA_BLOCK_HEIGHT=$DA_BLOCK_HEIGHT" >> restart-local.sh
-echo "NAMESPACE_ID=$NAMESPACE" >> restart-local.sh
+echo "NAMESPACE=$NAMESPACE" >> restart-local.sh
 echo "AUTH_TOKEN=$AUTH_TOKEN" >> restart-local.sh
 
 echo "gmd start --rollkit.aggregator true --rollkit.da_layer celestia --rollkit.da_config='{\"base_url\":\"http://localhost:26658\",\"timeout\":60000000000,\"fee\":600000,\"gas_limit\":6000000,\"auth_token\":\"'\$AUTH_TOKEN'\"}' --rollkit.namespace_id \$NAMESPACE --rollkit.da_start_height \$DA_BLOCK_HEIGHT --rpc.laddr tcp://127.0.0.1:36657 --p2p.laddr \"0.0.0.0:36656\"" >> restart-local.sh
