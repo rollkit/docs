3e5726ddc5991c538453b78a4c648ecbd59c7e18
Ganesha Upadhyaya <gupadhyaya@Ganeshas-MacBook-Pro-2.local>
2024-03-21 12:36:51 -0500

    emit AUTH_TOKEN to restart script

--- scripts/cosmwasm/init.sh
+++ scripts/cosmwasm/init.sh
@@ -57,6 +57,7 @@ jq --argjson pubKey "$PUB_KEY" '.consensus["validators"]=[{"address": "'$ADDRESS
 # create a restart-testnet.sh file to restart the chain later
 [ -f restart-wasmd.sh ] && rm restart-wasmd.sh
 echo "DA_BLOCK_HEIGHT=$DA_BLOCK_HEIGHT" >> restart-wasmd.sh
+echo "AUTH_TOKEN=$AUTH_TOKEN" >> restart-wasmd.sh
 
 echo "wasmd start --rollkit.aggregator --rollkit.da_auth_token=\$AUTH_TOKEN --rollkit.da_namespace 00000000000000000000000000000000000000000008e5f679bf7116cb --rollkit.da_start_height \$DA_BLOCK_HEIGHT --rpc.laddr tcp://127.0.0.1:36657 --grpc.address 127.0.0.1:9290 --p2p.laddr \"0.0.0.0:36656\" --minimum-gas-prices="0.025stake"" >> restart-wasmd.sh
 
