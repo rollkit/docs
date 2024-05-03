69b51fb16b86910c3818f27790510574d4193463
yarikbratashchuk <yarik.bratashchuk@gmail.com>
2024-05-03 11:06:30 +0200

    Make install to make build

--- scripts/install-mock-da.sh
+++ scripts/install-mock-da.sh
@@ -5,6 +5,6 @@ git clone https://github.com/rollkit/mock-da.git
 cd mock-da || { echo "Failed to find the downloaded repository"; exit 1; }
 git checkout $1
 echo "Building and installing Mock DA..."
-make install
+make build
 echo "Starting Mock DA..."
 ./build/mock-da
