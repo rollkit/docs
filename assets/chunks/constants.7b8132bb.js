0d135fe70ea0b8f9cedcc97fb29bd52d33b5bc64
YazzyYaz <yaz@celestia.org>
2024-05-09 19:06:28 +0000

    deploy: 74156f66076db235ab3ac62fa988430f10605425

---
+++ assets/chunks/constants.7b8132bb.js
@@ -0,0 +1,1 @@
+const o=Object.freeze({golangVersion:"1.22.2",rollkitLatestTag:"v0.13.3",rollkitLatestSha:"45b1573",rollkitCosmosSDKVersion:"v0.50.6-rollkit-v0.13.3-no-fraud-proofs",mockDALatestTag:"v0.1.0",igniteVersionTag:"v28.3.0"});export{o as c};
