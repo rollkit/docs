890d1ddd5c2f98833f71ba8b0b3b6c06f7f70164
jcstein <mail@jcstein.dev>
2024-05-16 15:02:27 -0400

    feat: remove duplicate mono

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -121,10 +121,6 @@ export default withMermaid({
       'link',
       { href: 'https://fonts.googleapis.com/css2?family=Ubuntu+Mono:ital,wght@0,400;0,700;1,400;1,700&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap', rel: 'stylesheet' }
     ],
-    [
-      'link',
-      { href: 'https://fonts.googleapis.com/css2?family=Ubuntu+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap', rel: 'stylesheet' }
-    ],
   ],
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
