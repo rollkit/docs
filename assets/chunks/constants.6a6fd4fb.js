2db7b6edddd9c122e5bf4c0ccaab1cfd24504370
yarikbratashchuk <yarikbratashchuk@users.noreply.github.com>
2024-06-14 16:42:13 +0000

    deploy: 488348fa1fd391e5c5c142496f38fa1c19d2eb01

---
+++ assets/chunks/constants.6a6fd4fb.js
@@ -0,0 +1,1 @@
+const o=Object.freeze({golangVersion:"go1.22.3",nodeVersion:"21.7.2",yarnVersion:"1.22.19",rollkitLatestTag:"v0.13.3",rollkitLatestSha:"45b1573",rollkitCosmosSDKVersion:"v0.50.6-rollkit-v0.13.3-no-fraud-proofs",localDALatestTag:"v0.2.0",igniteVersionTag:"v28.4.0"});export{o as c};
--- install-gm-rollup.sh
+++ install-gm-rollup.sh
@@ -3,6 +3,4 @@
 echo "Downloading GM tutorial rollup source code..."
 git clone https://github.com/rollkit/gm.git
 cd gm || { echo "Failed to find the downloaded repository"; exit 1; }
-git fetch && git checkout remotes/origin/tutorial-local-da
-echo "Building and configuring rollup chain..."
-bash init.sh
+git fetch && git checkout remotes/origin/tutorial-local-da-rollkit
