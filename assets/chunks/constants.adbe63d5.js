73589fdf5c572e31ae3db566094b012a345b05c4
Manav-Aggarwal <Manav-Aggarwal@users.noreply.github.com>
2024-06-20 20:49:09 +0000

    deploy: f4a47d308b3d10656b3197d4d6c9d890836b3512

---
+++ assets/chunks/constants.adbe63d5.js
@@ -0,0 +1,1 @@
+const o=Object.freeze({golangVersion:"go1.22.3",nodeVersion:"21.7.2",yarnVersion:"1.22.19",rollkitLatestTag:"v0.13.4",rollkitLatestSha:"45b1573",rollkitCosmosSDKVersion:"v0.50.6-rollkit-v0.13.3-no-fraud-proofs",rollkitIgniteAppVersion:"v0.2.1",localDALatestTag:"v0.2.0",igniteVersionTag:"v28.4.0"});export{o as c};
