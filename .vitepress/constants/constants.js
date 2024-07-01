59dc2c464ddfb2e66550ba28282f4b5807e0055b
Manav Aggarwal <manavaggarwal1234@gmail.com>
2024-07-01 18:49:59 +0400

    Update rollkit latest sha (#412)

--- .vitepress/constants/constants.js
+++ .vitepress/constants/constants.js
@@ -5,7 +5,7 @@ const constants = Object.freeze({
   yarnVersion: "1.22.19",
 
   rollkitLatestTag: "v0.13.5",
-  rollkitLatestSha: "45b1573",
+  rollkitLatestSha: "6a33192",
   rollkitCosmosSDKVersion: "v0.50.6-rollkit-v0.13.3-no-fraud-proofs",
   rollkitIgniteAppVersion: "v0.2.1",
 
