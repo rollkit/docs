7cda96a285ab3332382bdcf1fb824557bbea1b1d
Yarik Bratashchuk <yarik.bratashchuk@gmail.com>
2024-09-05 14:42:35 +0000

    Minor djustments

--- .vitepress/constants/constants.js
+++ .vitepress/constants/constants.js
@@ -4,12 +4,12 @@ const constants = Object.freeze({
   nodeVersion: "21.7.2",
   yarnVersion: "1.22.19",
 
-  rollkitLatestTag: "v0.13.6",
+  rollkitLatestTag: "v0.13.8",
   rollkitLatestSha: "6a33192",
   rollkitCosmosSDKVersion: "v0.50.6-rollkit-v0.13.3-no-fraud-proofs",
   rollkitIgniteAppVersion: "v0.2.1",
 
-  localDALatestTag: "v0.2.0",
+  localDALatestTag: "v0.3.1",
 
   igniteVersionTag: "v28.4.0",
 });
