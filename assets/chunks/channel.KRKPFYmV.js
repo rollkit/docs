e1ee71d04362d1625260b036f9c170d91413877e
MSevey <MSevey@users.noreply.github.com>
2024-10-08 13:34:53 +0000

    deploy: 1f673fbe726a64faff0d064c4e1c19b9d509f1df

--- assets/chunks/channel.KRKPFYmV.js
+++
@@ -1,1 +0,0 @@
-import{U as a,C as n}from"../app.Jkpy1I3y.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
--- assets/chunks/constants.CQ_TuUI4.js
+++
@@ -1,1 +0,0 @@
-const o=Object.freeze({golangVersion:"go1.22.3",nodeVersion:"21.7.2",yarnVersion:"1.22.19",rollkitLatestTag:"v0.13.8",rollkitLatestSha:"6a33192",rollkitCosmosSDKVersion:"v0.50.6-rollkit-v0.13.3-no-fraud-proofs",rollkitIgniteAppVersion:"v0.2.1",localDALatestTag:"v0.3.1",igniteVersionTag:"v28.4.0"});export{o as c};
