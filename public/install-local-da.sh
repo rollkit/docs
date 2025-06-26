6b75591f26361658040060f60965cb1a4e1a9ed8
pthmas <9058370+pthmas@users.noreply.github.com>
2025-06-26 17:53:54 +0200

    updated install local script

--- public/install-local-da.sh
+++ public/install-local-da.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo "Downloading Local-DA source code..."
-git clone https://github.com/rollkit/rollkit.git
+git clone  --depth=1 https://github.com/rollkit/rollkit.git
+# TODO : replace with go install once the repo is tagged
 cd rollkit || { echo "Failed to find the downloaded repository"; exit 1; }
 make build-da
 echo "Starting Local DA..."
