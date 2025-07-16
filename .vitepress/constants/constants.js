e26237916abfcfecbc9ca5d3335dfac2c795de62
pthmas <9058370+pthmas@users.noreply.github.com>
2025-07-16 15:56:38 +0200

    updated other versions and cleanup unused constants

--- .vitepress/constants/constants.js
+++ .vitepress/constants/constants.js
@@ -1,24 +1,19 @@
 const constants = Object.freeze({
   golangVersion: "go1.22.3",
 
-  nodeVersion: "21.7.2",
-  yarnVersion: "1.22.19",
-
   rollkitLatestTag: "v1.0.0-beta.1",
   rollkitLatestSha: "cd1970de",
   rollkitCosmosSDKVersion: "v0.50.6-rollkit-v0.13.3-no-fraud-proofs",
   rollkitIgniteAppVersion: "rollkit/v0.2.1",
 
-  localDALatestTag: "v0.3.1",
-  goSequencingLatestTag: "v0.4.1",
-  centralizedSequencerLatestTag: "v0.4.0",
+  localDALatestTag: "v1.0.0-beta.1",
   igniteVersionTag: "v28.5.3",
 
-  celestiaNodeArabicaTag: "v0.20.2-arabica",
+  celestiaNodeArabicaTag: "v0.23.4-arabica",
   celestiaNodeArabicaRollkitTag: "v1.0.0-beta.1",
-  celestiaNodeMochaTag: "v0.20.2-mocha",
+  celestiaNodeMochaTag: "v0.23.4-mocha",
   celestiaNodeMochaRollkitTag: "v1.0.0-beta.1",
-  celestiaNodeMainnetTag: "v0.17.2",
+  celestiaNodeMainnetTag: "v0.22.3",
   celestiaNodeMainnetRollkitTag: "v1.0.0-beta.1",
 });
 export default constants;
